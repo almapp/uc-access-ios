36e46e82670f1725a67f747aa3f731343f68266a
Patricio López <lopezjuripatricio@gmail.com>
2016-02-21 12:22:40 -0300

    progress

--- uc-access/InformationViewController.swift
+++ uc-access/InformationViewController.swift
@@ -9,10 +9,12 @@
 import Foundation
 import UIKit
 import Static
+import SwiftyJSON
 
 let FORM = "https://almapp.github.io/uc-access/form"
 let POLICY = "https://almapp.github.io/uc-access/policy"
 let REPO = "https://github.com/almapp/uc-access-ios"
+let APPS = "https://almapp.github.io/uc/apps.json"
 
 class InformationViewController: UITableViewController {
     var data: DataSource?
@@ -37,7 +39,7 @@ class InformationViewController: UITableViewController {
                 ], footer: "Resumen: Tus datos son guardados de manera segura en tu dispositivo y no son enviados a ninguna fuente externa, solo a las páginas que explícitamente seleccionas y requieren inicio de sesión. Es de tu responsabilidad tener tu dispositivo con clave"),
             
             Section(header: "Aplicación", rows: [
-                Row(text: "Versión", detailText: "1.0.0"),
+                Row(text: "Versión", detailText: "1.1.0"),
                 Row(text: "Licencia", detailText: "GPL-3.0"),
                 Row(text: "Repositorio", detailText: "Github", selection: { [unowned self] in
                     self.goToURL(REPO)
@@ -54,6 +56,30 @@ class InformationViewController: UITableViewController {
                     }, accessory: Row.Accessory.DisclosureIndicator),
                 ])
             ])
+
+        self.fetchAppsInfo()
+    }
+    
+    func fetchAppsInfo() {
+        Request.GET(APPS).then { (response, data) -> Void in
+            var section = Section(header: "Alma Máter 2")
+            JSON(data: data).arrayValue.forEach { app in
+                for platform in app["platforms"].arrayValue {
+                    if platform["platform"].stringValue == "ios" && platform["store"].stringValue.characters.count > 0 {
+                        section.rows.append(
+                            Row(text: app["name"].stringValue,
+                                detailText: app["description"].stringValue,
+                                cellClass: SubtitleCell.self,
+                                selection: { [unowned self] in
+                                    self.goToURL("https://itunes.apple.com/cl/app/mapas-uc/id1080468856?mt=8") //platform["store"].stringValue)
+                                },
+                                accessory: Row.Accessory.DisclosureIndicator)
+                        )
+                    }
+                }
+            }
+            self.data!.sections.append(section)
+        }
     }
     
     func goToURL(URL: String) {
